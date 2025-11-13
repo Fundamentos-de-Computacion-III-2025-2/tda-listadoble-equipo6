c5d51283860c3f8b8cf5a19f345871ccdf959230
ErnestoVzM <ernestovelazquez0077@gmail.com>
2025-11-13 12:53:15 -0800

    Avance 1 - Se asignan TODOs a cada integrante del equipo.

--- src/ListaDoble.java
+++ src/ListaDoble.java
@@ -2,28 +2,36 @@ public class ListaDoble {
     protected NodoDoble inicio, fin; //Apuntadores para saber donde esta el inicio y el fin de la lista doble
 
     //Constructor para crear la lista doble vacía
-    public ListaDoble(){
+    public ListaDoble() {
         inicio = null;
-        fin =null;
+        fin = null;
     }
 
     //Metodo para saber si la lista doble está vacía
-    public boolean listaVacia(){
-        if(inicio==null){
+    public boolean listaVacia() {
+        if (inicio == null) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    //Metodo para agregar un NodoDoble al Inicio de la Lista Doble
-    public void insertarInicio(int dato){
-
+    //Metodo para agregar un NodoDoble al Inicio de la Lista Doble ernesto
+    public void insertarInicio(int dato) {
+        if (listaVacia()) {
+          NodoDoble nuevo = new NodoDoble(dato);
+          inicio = nuevo;
+          fin = nuevo;
+        } else {
+            NodoDoble nuevo = new NodoDoble(dato, inicio, null);
+            inicio.anterior = nuevo;
+            inicio = nuevo;
+        }
 
     }
 
     //Metodo para insertar al Final de la lista doble
-    public void insertarFinal(int dato){
+    public void insertarFinal(int dato) {
 
     }
 
@@ -33,48 +41,70 @@ public class ListaDoble {
     número mayor al elemento que se encuentre en la lista, si no se encuentar un dato mayor
     se inserta al final */
 
-    public void insertarEnOrden(int dato){
+    public void insertarEnOrden(int dato) {
 
     }
 
 
-    //Eliminar al inicio
-    public int eliminarInicio(){
-
-        return -1;
+    //Eliminar al inicio Ernesto
+    public int eliminarInicio() {
+        if (listaVacia()) {
+            return -1;
+        }
+        int datoEliminado = inicio.dato;
+        if (inicio == fin) {
+            inicio = null;
+            fin = null;
+        } else {
+            inicio = inicio.siguiente;
+            inicio.anterior = null;
+        }
+        return datoEliminado;
     }
 
-    //Eliminar al final
-    public int eliminarFinal(){
+    //Eliminar al final ernesto
+    public int eliminarFinal() {
+        if (listaVacia()) {
+            return -1;
+        }
 
-        return -1;
+        int datoEliminado = fin.dato;
+
+        if (inicio == fin) {
+            inicio = null;
+            fin = null;
+        } else {
+            fin = fin.anterior;
+            fin.siguiente = null;
+        }
+        return datoEliminado;
     }
 
     //Eliminar un elemento
-    public int eliminarElemento(int elemento){
+    public int eliminarElemento(int elemento) {
 
         return elemento;
     }
 
     //Metodo para buscar un elemento
-    public boolean buscarElemento(int elemento){
+    public boolean buscarElemento(int elemento) {
 
         return false;
 
     }
 
     //Imprimir los datos de la lista doble de inicio a fin
-    public void mostrarInicioFin(){
-        NodoDoble actual=inicio;
+    public void mostrarInicioFin() {
+        NodoDoble actual = inicio;
         System.out.println();
-        while(actual!=null){
-            System.out.print(actual.dato+" --> ");
+        while (actual != null) {
+            System.out.print(actual.dato +"-->");
             actual = actual.siguiente;
         }
     }
 
     //Imprimir los datos de la lista doble de fin a inicio
-    public void mostrarFinInicio(){
+    public void mostrarFinInicio() {
 
     }
 
